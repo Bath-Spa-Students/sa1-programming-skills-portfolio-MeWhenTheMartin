b01b8f1d839d8357089d7a2543b54f647ffa5690
MartinEdu <MeWhenTheMartin@users.noreply.github.com>
2024-10-15 13:11:09 +0400

    Create Coding is Cool.py
    Coding is Cool

---
+++ Exercise 01/Coding is Cool.py
@@ -0,0 +1,20 @@
+# string concatenation [joining or combination of strings and values]
+
+# variables [w is for word]
+w1 = "Coding"
+w2 = "Is"
+w3 = "Cool"
+# these variables are values stored in a string arrangement
+
+# using operators to add variables
+print(w1 + w2 + w3)
+# using empty quotation marks with a space separates
+print(w1 + " " + w2 + " " + w3)
+# alternatively you can use commas to separate, because "," automatically adds a space after a variable
+print(w1,w2,w3)
+# examples of f-strings
+print(f"Coding {w2} Cool!")
+# f-strings is a format that looks cleaner to the eyes for most coders
+# this simplifies the layout and using "{}" will read a variable inside that bracket
+
+# Coding is Cool
